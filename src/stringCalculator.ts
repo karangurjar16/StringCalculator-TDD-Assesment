639dfb25d284bbb45a29baf69a7019df779b8939
Karan Gurjar <142745773+karangurjar16@users.noreply.github.com>
2025-07-05 00:49:43 +0530

    Step 6 :  Implement filter for number greater than 1000

--- src/stringCalculator.ts
+++ src/stringCalculator.ts
@@ -1,34 +1,36 @@
 export function add(input: string): number {
-  if (input === '') {
-    return 0;
-  }
+	if (input === '') {
+		return 0;
+	}
 
-  const { numbersString, delimiter } = extractDelimiterAndNumberString(input);
-  const numArray = numbersString.split(delimiter).map(n => parseInt(n, 10));
+	const { numbersString, delimiter } = extractDelimiterAndNumberString(input);
+	const numArray = numbersString.split(delimiter).map(n => parseInt(n, 10));
 
-  const negatives = numArray.filter(n => n < 0);
-  if (negatives.length > 0) {
-    throw new Error(`negative numbers not allowed ${negatives.join(',')}`);
-  }
+	const negatives = numArray.filter(n => n < 0);
+	if (negatives.length > 0) {
+		throw new Error(`negative numbers not allowed ${negatives.join(',')}`);
+	}
 
-  return numArray.reduce((sum, n) => sum + n, 0);
+	const filtered = numArray.filter(n => n <= 1000);
+
+	return filtered.reduce((sum, n) => sum + n, 0);
 }
 
 function extractDelimiterAndNumberString(input: string): { delimiter: RegExp; numbersString: string } {
-  let delimiter: RegExp = /,|\n/;
-  let numbersString = input;
+	let delimiter: RegExp = /,|\n/;
+	let numbersString = input;
 
-  if (input.startsWith('//')) {
-    const newlineIndex = input.indexOf('\n');
-    const delimiterSection = input.substring(2, newlineIndex);
-    numbersString = input.substring(newlineIndex + 1);
+	if (input.startsWith('//')) {
+		const newlineIndex = input.indexOf('\n');
+		const delimiterSection = input.substring(2, newlineIndex);
+		numbersString = input.substring(newlineIndex + 1);
 
-    const escapedDelimiter = delimiterSection.replace(/[.*+?^${}()|[\]\\]/g, '\\$&');
-    delimiter = new RegExp(escapedDelimiter);
-  }
+		const escapedDelimiter = delimiterSection.replace(/[.*+?^${}()|[\]\\]/g, '\\$&');
+		delimiter = new RegExp(escapedDelimiter);
+	}
 
-  return {
-    delimiter,
-    numbersString,
-  };
+	return {
+		delimiter,
+		numbersString,
+	};
 }
