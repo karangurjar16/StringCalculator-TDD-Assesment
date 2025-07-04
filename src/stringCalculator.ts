69e36e25d805f05e8d34732a30b9bfd4aab615b7
Karan Gurjar <142745773+karangurjar16@users.noreply.github.com>
2025-07-04 23:21:54 +0530

    Step 4 : Implement handling to return sum of custom single character as a delimiter in string calculator

--- src/stringCalculator.ts
+++ src/stringCalculator.ts
@@ -3,9 +3,28 @@ export function add(input: string) {
         return 0;
     }
 
-    let delimiters: RegExp = /,|\n/;
-    const numArray = input.split(delimiters);
+    const { numbersString, delimiter } = extractDelimiterAndNumberString(input);
+    const numArray = numbersString.split(delimiter);
 
     return numArray.reduce((sum, x) => sum + parseInt(x, 10), 0);
 
+}
+
+function extractDelimiterAndNumberString(input: string): { delimiter: RegExp; numbersString: string } {
+  let delimiter: RegExp = /,|\n/;
+  let numbersString = input;
+
+  if (input.startsWith('//')) {
+    const newlineIndex = input.indexOf('\n');
+    const delimiterSection = input.substring(2, newlineIndex);
+    numbersString = input.substring(newlineIndex + 1);
+
+    const escapedDelimiter = delimiterSection.replace(/[.*+?^${}()|[\]\\]/g, '\\$&');
+    delimiter = new RegExp(escapedDelimiter);
+  }
+
+  return {
+    delimiter,
+    numbersString,
+  };
 }
