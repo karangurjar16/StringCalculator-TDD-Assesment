2c6193e9f209125596edf1e30e9687a70ab98534
Karan Gurjar <142745773+karangurjar16@users.noreply.github.com>
2025-07-04 22:24:33 +0530

    Step 1 : Implement handling to return two comma-seprated numbers

--- src/stringCalculator.ts
+++ src/stringCalculator.ts
@@ -1,9 +1,14 @@
-export function add(numbers: string) {
-    if (numbers === '') {
+export function add(input: string) {
+    if (input === '') {
         return 0;
     }
 
-    const num = parseInt(numbers, 10);
-    return isNaN(num) ? 0 : num;
+    const numbers = input.split(',');
+
+    if (numbers.length === 1) {
+        return parseInt(numbers[0], 10);
+    }
+
+    return parseInt(numbers[0], 10) + parseInt(numbers[1], 10);
 
 }
