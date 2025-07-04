27b28b9e644864e2c323f025ad35477a51e6f3dc
Karan Gurjar <142745773+karangurjar16@users.noreply.github.com>
2025-07-05 01:14:44 +0530

    Step 7 : Implement handling to return of sum support for long custom delimiter in string calculator

--- src/stringCalculator.ts
+++ src/stringCalculator.ts
@@ -16,21 +16,25 @@ export function add(input: string): number {
 	return filtered.reduce((sum, n) => sum + n, 0);
 }
 
-function extractDelimiterAndNumberString(input: string): { delimiter: RegExp; numbersString: string } {
-	let delimiter: RegExp = /,|\n/;
-	let numbersString = input;
+export function extractDelimiterAndNumberString(input: string): { delimiter: RegExp; numbersString: string } {
+  let delimiter: RegExp = /,|\n/;
+  let numbersString = input;
 
-	if (input.startsWith('//')) {
-		const newlineIndex = input.indexOf('\n');
-		const delimiterSection = input.substring(2, newlineIndex);
-		numbersString = input.substring(newlineIndex + 1);
+  if (input.startsWith('//')) {
+    const newlineIndex = input.indexOf('\n');
+    let delimiterSection = input.substring(2, newlineIndex);
+    numbersString = input.substring(newlineIndex + 1);
 
-		const escapedDelimiter = delimiterSection.replace(/[.*+?^${}()|[\]\\]/g, '\\$&');
-		delimiter = new RegExp(escapedDelimiter);
-	}
+    if (delimiterSection.startsWith('[') && delimiterSection.endsWith(']')) {
+      delimiterSection = delimiterSection.slice(1, -1); 
+    }
 
-	return {
-		delimiter,
-		numbersString,
-	};
+    const escapedDelimiter = delimiterSection.replace(/[.*+?^${}()|[\]\\]/g, '\\$&');
+    delimiter = new RegExp(escapedDelimiter);
+  }
+
+  return {
+    delimiter,
+    numbersString,
+  };
 }
