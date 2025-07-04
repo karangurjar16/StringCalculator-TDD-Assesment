ea9448fbd62ace2fe9bf82a039f8188ce950cacd
Karan Gurjar <142745773+karangurjar16@users.noreply.github.com>
2025-07-04 22:12:58 +0530

    Step1 : Add failing test  for single number input in string calculator

--- src/tests/stringCalculator.test.ts
+++ src/tests/stringCalculator.test.ts
@@ -10,6 +10,11 @@ describe('String Calculator', () => {
             expect(add('')).toBe(0);
         });
 
+        it('should return the number itself for single number', () => {
+            expect(add('1')).toBe(1);
+            expect(add('5')).toBe(5);
+        });
+
     });
-    
+
 });
